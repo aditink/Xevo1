cdd687757a4b774d3f263d7e18cc70cd2953e1ef
Sam Thomas <sgpthomas@gmail.com>
2017-11-10 16:00:29 -0500

    made preliminary quick hit ui

--- Xevo1/app/src/main/java/xevo/xevo1/QuickHit.kt
+++ Xevo1/app/src/main/java/xevo/xevo1/QuickHit.kt
@@ -12,11 +12,6 @@ class QuickHit : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quick_hit)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
 }
