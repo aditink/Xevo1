78c018de920c38a363ab2dce11ad24a5af10a1f9
Sam Thomas <sgpthomas@gmail.com>
2017-11-11 20:52:31 -0500

    fixed the fonts for android 8.0

--- Xevo1/app/src/main/java/xevo/xevo1/MainActivity.kt
+++ Xevo1/app/src/main/java/xevo/xevo1/MainActivity.kt
@@ -16,11 +16,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-//        fab.setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                    .setAction("Action", null).show()
-//        }
     }
 
     fun openQuickHitPage(view: View) {
