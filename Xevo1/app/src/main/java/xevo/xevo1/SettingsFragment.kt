0c7aabbae1d91870ce55d2de34271899a757cd93
aditi <ank55@cornell.edu>
2017-12-21 01:06:49 +0530

    Facebook login set up. Not connected to firebase yet

--- Xevo1/app/src/main/java/xevo/xevo1/SettingsFragment.kt
+++ Xevo1/app/src/main/java/xevo/xevo1/SettingsFragment.kt
@@ -33,7 +33,7 @@ class SettingsFragment : XevoFragment() {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val v = inflater!!.inflate(R.layout.fragment_settings, container, false)
