b2e5a257b1872af004c400a5415b768816597802
aditi <ank55@cornell.edu>
2017-12-22 00:55:45 +0530

    added empty activity for consuktant questions screen

---
+++ Xevo1/app/src/main/java/xevo/xevo1/ConsultantQuestions.kt
@@ -0,0 +1,17 @@
+package xevo.xevo1
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_consultant_questions.*
+
+class ConsultantQuestions : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consultant_questions)
+        setSupportActionBar(toolbar)
+    }
+
+}
