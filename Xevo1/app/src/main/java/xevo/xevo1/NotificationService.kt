c681e9ebf02ccbfdc5d4d2a5464a30fd43325e1b
aditi <ank55@cornell.edu>
2018-01-20 23:40:42 +0530

    Pared NotificationService to bare bones, working for API 26 now

--- Xevo1/app/src/main/java/xevo/xevo1/NotificationService.kt
+++ Xevo1/app/src/main/java/xevo/xevo1/NotificationService.kt
@@ -1,5 +1,6 @@
 package xevo.xevo1
 
+import android.annotation.TargetApi
 import android.app.NotificationChannel
 import android.app.Service
 import android.content.Intent
@@ -12,6 +13,9 @@ import android.app.NotificationManager
 import android.media.RingtoneManager
 import android.app.PendingIntent
 import android.content.Context
+import android.graphics.Color
+import android.os.Build
+import android.support.annotation.RequiresApi
 import android.support.design.widget.CoordinatorLayout.Behavior.setTag
 import android.support.v4.app.NotificationCompat
 
@@ -19,6 +23,7 @@ import android.support.v4.app.NotificationCompat
 class NotificationService : FirebaseMessagingService() {
 
     private val TAG = "MyFirebaseMsgService"
+    private val FOLLOWER_CHANNEL = "FOLLOWER_CHANNEL"
 
     /**
      * Called when notification
@@ -26,108 +31,60 @@ class NotificationService : FirebaseMessagingService() {
      *
      * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
      */
-    // [START receive_message]
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
-        // [START_EXCLUDE]
-        // There are two types of messages data messages and notification messages. Data messages are handled
-        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
-        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
-        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
-        // When the user taps on the notification they are returned to the app. Messages containing both notification
-        // and data payloads are treated as notification messages. The Firebase console always sends notification
-        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
-        // [END_EXCLUDE]
-
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: " + remoteMessage!!.from!!)
-
-        // Check if message contains a data payload.
-        if (remoteMessage.data.size > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.data)
-
-            if (/* Check if data needs to be processed by long running job */ false) {
-                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-                scheduleJob()
-            } else {
-                // Handle message within 10 seconds
-                handleNow()
-            }
-
-        }
-
-        // Check if message contains a notification payload.
-        if (remoteMessage.notification != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.notification!!.body!!)
-        }
-
+        // TODO: Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
         // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
-    }
-    // [END receive_message]
-
-    /**
-     * Schedule a job using FirebaseJobDispatcher.
-     */
-    private fun scheduleJob() {
-        // [START dispatch_job]
-//        val dispatcher = FirebaseJobDispatcher(GooglePlayDriver(this))
-//        val myJob = dispatcher.newJobBuilder()
-//                .setService(MyJobService::class.java)
-//                .setTag("my-job-tag")
-//                .build()
-//        dispatcher.schedule(myJob)
-        // [END dispatch_job]
+        // message, here is where that should be initiated.
+        Log.d(TAG, "From: " + remoteMessage!!.from)
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.notification?.body!!)
+        sendNotification(remoteMessage)
+        val intent = Intent(this@NotificationService, Main::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.putExtra("message", remoteMessage.notification?.body!!)
+        startActivity(intent)
     }
 
-    /**
-     * Handle time allotted to BroadcastReceivers.
-     */
-    private fun handleNow() {
-        sendNotification("Answer ready!")
-        Log.d(TAG, "Short lived task is done.")
-    }
-
-    /**
-     * Create and show a simple notification containing the received FCM message.
-     *
-     * @param messageBody FCM message body received.
-     */
-    private fun sendNotification(messageBody: String) {
+    @TargetApi(Build.VERSION_CODES.O)
+    private fun sendNotification(remoteMessage: RemoteMessage) {
         val intent = Intent(this, Main::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */,
-                intent, PendingIntent.FLAG_ONE_SHOT)
-
-        val channelId = getString(R.string.default_notification_channel_id)
-//        var notificationChannel = object : NotificationChannel(channelId, "Xevo",
-//                NotificationManager.IMPORTANCE_DEFAULT){}
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT)
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-//        val notificationBuilder = NotificationCompat.Builder(this, channelId)
-//                .setSmallIcon(R.drawable.ic_stat_ic_notification)
-//                .setContentTitle("FCM Message")
-//                .setContentText(messageBody)
-//                .setAutoCancel(true)
-//                .setSound(defaultSoundUri)
-//                .setContentIntent(pendingIntent)
-
-        val notificationBuilder = object : NotificationCompat.Builder(this) {}
-        notificationBuilder.setSmallIcon(R.drawable.xevo_logo)
-                .setContentTitle("FCM Message")
-                .setContentText(messageBody)
-                .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent)
-
-
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        //if 26 or above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val followersChannel = NotificationChannel(
+                    FOLLOWER_CHANNEL,
+                    "Follower Notification Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT)
 
-        // Sets an ID for the notification
-        val mNotificationId = 1
-// Gets an instance of the NotificationManager service
-        val mNotifyMgr = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-// Builds the notification and issues it.
-        mNotifyMgr.notify(mNotificationId, notificationBuilder.build())
-//        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+// Configure the channel's initial settings
+            followersChannel.lightColor = Color.GREEN
+            followersChannel.vibrationPattern = longArrayOf(100, 100)
+
+// Submit the notification channel object to the notification manager
+            notificationManager.createNotificationChannel(followersChannel)
+
+            val notificationBuilder = NotificationCompat.Builder(this, FOLLOWER_CHANNEL)
+                    .setContentText(remoteMessage.notification?.body)
+                    .setAutoCancel(true)
+                    .setSmallIcon(R.drawable.xevo_logo)
+                    .setSound(defaultSoundUri)
+                    .setContentIntent(pendingIntent)
+
+            notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+        }
+        else {
+            val notificationBuilder = NotificationCompat.Builder(this)
+                    .setContentText(remoteMessage.notification?.body)
+                    .setAutoCancel(true)
+                    .setSmallIcon(R.drawable.xevo_logo)
+                    .setSound(defaultSoundUri)
+                    .setContentIntent(pendingIntent)
+
+            notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+        }
     }
 }
